156bc28a7a7a1872c966290467d7754a06715ea6
Grace Sutherland <rgs1690@gmail.com>
2022-02-24 07:17:18 -0600

    update

--- BadSuperbowlNamer.Tests/NumberConverterTests.cs
+++ BadSuperbowlNamer.Tests/NumberConverterTests.cs
@@ -17,11 +17,53 @@ namespace BadSuperbowlNamer.Tests
             var expectedOutcome = "I";
 
             // Act => the actual thing we are testing, do this thing, when this happens...
-            var actualOutcome = NumberConverter.Convert(1);
+            var converter = new NumberConverter();
+            var actualOutcome = converter.Convert(numberToTest);
             // Assert lay out the expectations in such a way thatm, if not met, exceptions are thrown, then this happens...
-            // Assert.Equal(expectedOutcome, actualOutcome);
-            Assert.True(expectedOutcome == actualOutcome);
+             Assert.Equal(expectedOutcome, actualOutcome); // gives a better message when failure occurs
+             // Assert.True(expectedOutcome == actualOutcome); // doesn't give a very good message when failure occurs. 
         
         }
+        [Fact]
+        public void Number_Four_Is_Converted_To_Roman_Numeral_Correctly()
+        {
+            //arrange
+            var numbersToTest = 4;
+            var expectedOutcome = "IV";
+            //act
+            var converter = new NumberConverter();
+            var actualOutcome = converter.Convert(numbersToTest);
+
+            //assert
+            Assert.Equal(expectedOutcome, actualOutcome);
+
+}
+
+
+        //to debug a theory test with a specific set of inline data,
+        //set your breakpoint, then right-click the specific test in the Test Explorer, and choose debug
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(1, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [Theory]
+        public void Numbers_Can_Be_Converted_To_Roman_Numerals_Correctly(int numberToTest, string expectedOutcome )
+        {
+            ////arrange
+            //numbersToTest = 4;
+            //var expectedOutcome = "IV";
+            //act
+            var converter = new NumberConverter();
+            var actualOutcome = converter.Convert(numberToTest);
+
+            //assert
+            Assert.Equal(expectedOutcome, actualOutcome);
+
+        }
+
     }
 }
--- BadSuperbowlNamer/NumberConverter.cs
+++ BadSuperbowlNamer/NumberConverter.cs
@@ -8,13 +8,58 @@ namespace BadSuperbowlNamer
 {
     public class NumberConverter
     {
-        public static string Convert(int num)
+        public string Convert(int num)
         {
-           if (num == 1)
+            //length of number and pattern of 1,4,5,9 can somehow be tied together
+            //loop over all the digits in num
+
+
+
+
+            //any single digit number
+            char oneCharacter = 'I';
+            char fiveCharacter = 'V';
+            char tenCharacter = 'X';
+
+            var returnValue = "";
+
+            var newNumber = num % 9;
+            var numberOfTenCharacters = num / 9;
+            returnValue += new string(tenCharacter, numberOfTenCharacters);
+
+            if (newNumber == 0)
             {
-                return "I";
+                return oneCharacter + returnValue;
             }
-            return "";
+
+            newNumber = num % 5;
+            var numberOfFiveCharacters = num / 5;
+            returnValue += new string(fiveCharacter, numberOfFiveCharacters);
+
+            if (newNumber == 0)
+            {
+                return returnValue;
+            }
+
+            newNumber = num % 4; //num needs to be newNumber
+            numberOfFiveCharacters = num / 4;
+            returnValue += new string(fiveCharacter, numberOfFiveCharacters);
+
+            if (newNumber == 0)
+            {
+                returnValue = oneCharacter + returnValue;
+            }
+
+            newNumber = num % 1;
+            var numberOfOneCharacters = num / 1;
+            returnValue += new string(fiveCharacter, numberOfFiveCharacters);
+
+
+            return returnValue;
+
+
+            //translate that digit to a string roman numeral
+            //stick the numerals together
         }
     }
 }
